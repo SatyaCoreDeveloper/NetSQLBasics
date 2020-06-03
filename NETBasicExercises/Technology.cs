bd11e763b546d76a5a39dba53fa2c2538f302d22
sarath1732 <66354107+sarath1732@users.noreply.github.com>
2020-06-03 20:16:35 +0530

    Add files via upload

---
+++ NETBasicExercises/Technology.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School
+{
+    class Technology
+    {
+        private int technologyID;
+        private string technologyName;
+
+        public int TechnologyID
+        {
+            get
+            {
+                return technologyID;
+            }
+            private set
+            {
+                technologyID = value;
+            }
+        }
+
+        public string TechnologyName
+        {
+            get
+            {
+                return technologyName;
+            }
+            private set
+            {
+                technologyName = value;
+            }
+        }
+
+        public void print()
+        {
+            TechnologyID = 10;
+            TechnologyName = "DotNet";
+
+            Console.WriteLine("The ID of {0} is {1}", TechnologyName, TechnologyID);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NETBasicExercises/course.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School
+{
+    class course
+    {
+        private string title;
+        private int credits;
+        private string technology;
+        private int courseID;
+
+        public int CourseID
+        {
+            get
+            {
+                return courseID;
+            }
+            set
+            {
+                courseID = value;
+            }
+        }
+
+        public int Credits
+        {
+            get
+            {
+                return credits;
+            }
+            set
+            {
+                credits = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Technology
+        {
+            get
+            {
+                return technology;
+            }
+            set
+            {
+                technology = value;
+            }
+        }
+
+        public void print()
+        {
+            Title = "Asp.net";
+            Technology = "Dotnet";
+            Credits = 4;
+            CourseID = 100;
+
+            Console.WriteLine("{0} has {1} credits", Technology, Credits);
+            Console.ReadKey();
+        }
+    }
+}
